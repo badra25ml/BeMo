39247252e4b6132f911bbd12cff4dda359985e29
Aliou Maiga <badrasadja@gmail.com>
2020-07-27 01:06:43 -0700

    added noindex function, picture change function

--- public/javascripts/addedFunctions.js
+++ public/javascripts/addedFunctions.js
@@ -1,19 +1,38 @@
 window.onload = function(){
-  document.getElementById('change').onclick = function test(e){
-    console.log(e)
-    var pict = new XMLHttpRequest();
-    console.log(pict)
-    pict.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       document.getElementById('featureImg').innerHTML = this.responseText;
-      }
-    };
-    pict.open("GET", "ajax_info.txt", true);
-    pict.send();
+  // change picture
 
-    // Change images
-    function changeImage(){
-      document.getElementById('featureImg').src = pict;
-    };
+document.getElementById('change').onclick = function() {
+
+  const xhr = new XMLHttpRequest(),
+      method = "GET",
+      url = "/";
+
+  xhr.open(method, url, true);
+  xhr.onreadystatechange = function () {
+    if(xhr.readyState === XMLHttpRequest.DONE) {
+      var status = xhr.status;
+      if (status === 0 || (status >= 200 && status < 400)) {
+        // The request has been completed successfully
+        document.getElementById("featureImg").src = "images/contact-us.png"
+      } else {
+        // Oh no! There has been an error with the request!
+      }
+    }
+  };
+  xhr.send();
+ }
+ // Setting meta to noindex
+
+document.getElementById("switcher").onclick = function(){
+var x = document.getElementsByTagName("META");
+var txt = "";
+var i;
+for (i = 0; i < x.length; i++) {
+  if( x[i].name === 'robots' && x[i].content !== 'noindex'){
+    x[i].content = 'noindex';
+  }else{
+
+   }
   }
+ }
 };
